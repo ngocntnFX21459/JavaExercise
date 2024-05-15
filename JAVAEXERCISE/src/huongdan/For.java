599e90cbfb13909fa3c140caba9cd45fd2b9efdb
ngocntnFX21459 <168076920+ngocntnFX21459@users.noreply.github.com>
2024-05-15 10:09:20 +0700

    Update For.java

--- JAVAEXERCISE/src/huongdan/For.java
+++ JAVAEXERCISE/src/huongdan/For.java
@@ -2,7 +2,7 @@ package huongdan;
 
 import java.util.Scanner;
 
-public class For {
+public class VongLapFor {
 public static void main (String[] args) {
 	Scanner sc = new Scanner(System.in);
 	int tongchan=0;
